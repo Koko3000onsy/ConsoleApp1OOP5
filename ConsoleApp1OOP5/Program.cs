a6a86e75fb58e39c3c8bd8c43d0c976027333d9c
Koko3000onsy <onsykoko79@gmail.com>
2026-03-04 15:23:37 +0200

    Q2

--- ConsoleApp1OOP5/Program.cs
+++ ConsoleApp1OOP5/Program.cs
@@ -12,6 +12,37 @@
             //Q1.3 : Enable polymorphism without inheritance , Remove tight coupling between classes , Enable multiple inheritance(behavior)
             //Interfaces allow coding against behavior, not classes
             #endregion
+            #region Q2
+            //The problem is that both interfaces contain a method with the same name and signature
+            //Explicit implementation
+            //    interface IEnglishSpeaker
+            //{
+            //    void Greet();
+            //}
+
+            //interface IArabicSpeaker
+            //{
+            //    void Greet();
+            //}
+
+            //class Translator : IEnglishSpeaker, IArabicSpeaker
+            //{
+            //     void IEnglishSpeaker. Greet()
+            //    {
+            //        Console.WriteLine("Hello ");
+            //    }
+            //    void IArabicSpeaker.Greet()
+            //    {
+            //        Console.WriteLine(" Ahlan");
+            //    }
+            //}
+            //we cant call translator.Greet() because it void IArabicSpeaker.Greet() and void IEnglishSpeaker. Greet() are not puplic can access them in interface only
+            //translator t = new Translator();
+            //IArabicSpeaker arabicSpeaker = t;
+            //IEnglishSpeaker englishSpeaker = t;
+            //arabicSpeaker.Greet(); // Output: Ahlan
+            //englishSpeaker.Greet(); // Output: Hello
+            #endregion
         }
     }
 }
