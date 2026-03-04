92978843cde334fa0b66e87522738090285677fb
Koko3000onsy <onsykoko79@gmail.com>
2026-03-04 14:56:44 +0200

    Q1

--- ConsoleApp1OOP5/Program.cs
+++ ConsoleApp1OOP5/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Q1.1 :interface is a contract that defines a set of methods and properties that a class must implement.
+            //It specifies what a class can do, not how it does it.
+            //It cannot contain any implementation code. Interface = Promise, not implementation
+            //Q1.2 : We use interfaces instead of depending on concrete classes to make our code more flexible, maintainable, and scalable.
+            //Q1.3 : Enable polymorphism without inheritance , Remove tight coupling between classes , Enable multiple inheritance(behavior)
+            //Interfaces allow coding against behavior, not classes
+            #endregion
         }
     }
 }
